a9649a4f0799280471e8b2a246bfb7ca25edf7ab
IraMira333 <irinat333@gmail.com>
2023-04-28 16:43:54 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const containerGalleryImages = document.querySelector(".gallery");
+const imagesMarkup = creatFotoCardsMurkup(galleryItems);
+
+containerGalleryImages.insertAdjacentHTML("beforeend", imagesMarkup);
+
+function creatFotoCardsMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+        `;
+    })
+    .join("");
+}
+//! Creating a new SimpleLightbox instance and configuring it to use the "alt" attribute for captions
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
