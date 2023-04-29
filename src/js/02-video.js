e10b02ff6342d5d5670f50d33a443cd6ad99b7ed
IraMira333 <irinat333@gmail.com>
2023-04-29 22:55:49 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', function() {
+        console.log('played the video!');
+        player.getCurrentTime().then(function(currentTime) {
+          console.log(`time: `, currentTime)
+
+        });
+
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
 
