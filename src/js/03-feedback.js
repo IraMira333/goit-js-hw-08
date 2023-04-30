9be0067c5083334f6e37797d1f8fc5dda8037dbd
IraMira333 <irinat333@gmail.com>
2023-04-30 23:56:38 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector(`.feedback-form`),
+  email: document.getElementsByName(`email`),
+  feedback: document.getElementsByName(`message`),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', onFeedbackFormInput);
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFeedbackFormInput(evt) {
+  const email = evt.target.value;
+}
