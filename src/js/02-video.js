171a911f2601a09c5a9c5be4f7970df55b9bfc98
IraMira333 <irinat333@gmail.com>
2023-05-02 19:59:48 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,30 +7,15 @@ const player = new Player(iframe);
 player.on(
   'timeupdate',
   throttle(function () {
-    console.log('played the video!');
     player.getCurrentTime().then(function (currentTime) {
-      console.log('played slow the video!');
       localStorage.setItem('videoplayer-current-time', currentTime);
     });
   }, 1000)
 );
-
-player
-  .setCurrentTime(localStorage.getItem(`videoplayer-current-time`))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+if (localStorage.getItem(`videoplayer-current-time`)) {
+  console.log(`Відео вже почали дивитись`);
+  player.setCurrentTime(localStorage.getItem(`videoplayer-current-time`));
+}
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
