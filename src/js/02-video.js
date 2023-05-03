de00ef78af01bc33caca2222fc0541d6ab136c6e
IraMira333 <irinat333@gmail.com>
2023-05-03 20:31:03 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,16 +7,10 @@ const player = new Player(iframe);
 player.on(
   'timeupdate',
   throttle(function () {
-    player.getCurrentTime().then(function (currentTime) {
-      localStorage.setItem('videoplayer-current-time', currentTime);
-    });
+    player.getCurrentTime();
   }, 1000)
 );
 if (localStorage.getItem(`videoplayer-current-time`)) {
   console.log(`Відео вже почали дивитись`);
   player.setCurrentTime(localStorage.getItem(`videoplayer-current-time`));
 }
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
